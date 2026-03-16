da196bc316b3af8f9218bae8d8d741ebb881b465
gianluca andreoni <gianluca.andreoni@S2E-Pro.com>
2026-03-16 12:02:36 +0100

    Refactor navbar and apply improved styling.
    Reorganized and enhanced the navbar structure with new CSS classes for a more polished design. Added logo images, updated font families via Google Fonts, and introduced a background image for the app. These changes improve the overall visual hierarchy and user experience.

--- src/components/MenuLinks.jsx
+++ src/components/MenuLinks.jsx
@@ -3,9 +3,14 @@ export default function MenuLinks() {
         <section>
             <h2>I nostri menù</h2>
             <ul>
-                <li><a href="#">Menù trattoria</a></li>
-                <li><a href="#">Menù pizzeria</a></li>
-                <li><a href="#">Menù senza glutine</a></li>
+                <li>
+                    <a href="#">Menù trattoria</a>
+                </li>
+                <li>
+                    <a href="#">Menù pizzeria</a></li>
+                <li>
+                    <a href="#">Menù senza glutine</a>
+                </li>
             </ul>
         </section>
     );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import "./Navbar.css";
+import logo_image from "../../assets/images/logo.png";
+
 export default function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Pizzeria</li>
-                <li>Contatti</li>
-            </ul>
+        <nav className="navbar">
+            <div className="logo-container">
+                <img src={logo_image} alt="Logo Busca Vida" className="logo-image" />
+                <div className="logo-text">
+                    <h1 className="logo-text-busca">Busca</h1>
+                    <h1 className="logo-text-vida">Vida</h1>
+                </div>
+            </div>
+
+            <div className="menu-container">
+                <ul className="navbar-menu">
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>Contatti</li>
+                </ul>
+            </div>
         </nav>
     );
 }
